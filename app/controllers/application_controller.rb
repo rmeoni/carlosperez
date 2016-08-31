8fdb2a25ad915cdf597206aa8137cbdd607ce2f6
rmeoni <rodrigo.me13@gmail.com>
2016-08-31 09:40:07 -0400

    login and signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,54 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+	helper_method :current_user
+	helper_method :message
+
+
+	protect_from_forgery with: :exception
+	
+	def current_user
+	@current_user ||= User.find(session[:user_id]) if session[:user_id] 
+	end
+
+	def require_user
+	 unless current_user
+	 	flash[:error] = "Oops! You must be logged in to access this section."
+	 	redirect_to '/login'
+	 end
+	end
+
+	def require_editor
+	 unless current_user.editor? || current_user.admin?
+	 	flash[:error] = "Oops! You don't have permissions to access this section."
+	 	redirect_to '/'
+	 end
+	end
+
+	def require_admin
+		unless current_user.admin?
+			flash[:error] = "Oops! You don't have permissions to access this section."
+			redirect_to '/'
+		end
+	end
+
+	def require_super
+		unless current_user.super?
+			flash[:error] = "Oops! You don't have permissions to access this section."
+			redirect_to '/'
+		end
+	end
+
+	def user
+		@user = User.find(params[:id])
+	end
+
+	def require_owner
+		unless  
+			current_user.super? || current_user.id == user.id
+			redirect_to '/'
+		end 
+	end
+
+
 end
